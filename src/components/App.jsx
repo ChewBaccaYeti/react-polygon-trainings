1a4226979cd67768f0c07aec4ae5e89e7b71b119
Venedykt Mitishov <60115053+ChewBaccaYeti@users.noreply.github.com>
2023-08-01 20:06:09 +0000

    default image

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Painting } from 'components/Painting';
+import { paintings } from './paintings.json';
+
 export default function App() {
   return (
     <div
@@ -7,10 +10,38 @@ export default function App() {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <Painting
+        imgUrl={paintings[0].imgUrl}
+        title={paintings[0].title}
+        author={paintings[0].author}
+        quantity={paintings[0].quantity}
+        price={paintings[0].price}
+      />
+      <Painting
+        imgUrl={paintings[1].imgUrl}
+        title={paintings[1].title}
+        author={paintings[1].author}
+        quantity={paintings[1].quantity}
+        price={paintings[1].price}
+      />
+      <Painting
+        imgUrl={paintings[2].imgUrl}
+        title={paintings[2].title}
+        author={paintings[2].author}
+        quantity={paintings[2].quantity}
+        price={paintings[2].price}
+      />
+      <Painting
+        imgUrl={paintings[3].imgUrl}
+        title={paintings[3].title}
+        author={paintings[3].author}
+        quantity={paintings[3].quantity}
+        price={paintings[3].price}
+      />
     </div>
   );
-};
+}
--- src/components/Painting.jsx
+++ src/components/Painting.jsx
@@ -1,46 +1,65 @@
-const painting = (
-    <div>
-      <img src={data.url} alt={data.title} />
-      <h2>{data.title}</h2>
-      <p>
-        Author: <a href={'data.author.url'}>{data.author.tag}</a>
-      </p>
-      <p>Price: {data.price} credits</p>
-      <p>Quantity: {data.quantity}</p>
-      <button></button>
-    </div>
-  );
+// const painting = (
+//     <div>
+//       <img src={data.url} alt={data.title} />
+//       <h2>{data.title}</h2>
+//       <p>
+//         Author: <a href={'data.author.url'}>{data.author.tag}</a>
+//       </p>
+//       <p>Price: {data.price} credits</p>
+//       <p>Quantity: {data.quantity}</p>
+//       <button></button>
+//     </div>
+//   );
   
-  console.log(painting);
+//   console.log(painting);
   
-  function Painting() {
+//   function Painting() {
+//     return (
+//       <div>
+//         <img src={data.url} alt={data.title} />
+//         <h2>{data.title}</h2>
+//         <p>
+//           Author: <a href={'data.author.url'}>{data.author.tag}</a>
+//         </p>
+//         <p>Price: {data.price} credits</p>
+//         <p>Quantity: {data.quantity}</p>
+//         <button></button>
+//       </div>
+//     );
+//   }
+  
+//   ReactDOM.render(painting, document.querySelector('#root'));
+  
+//   const example = example[0];
+  
+//   ReactDOM.render(
+//     <Painting
+//       url={example.url}
+//       a="x+2=5"
+//       b="y-3=2"
+//       c="z-(1+1)=3"
+//       d={{ e: 7 }}
+//       f={10}
+//       hint="This is props."
+//     />,
+//     document.querySelector('#root')
+//   );
+
+import defaultImage from './default.jpg';
+
+  export const Painting = ({ imgUrl, title, quantity, author, avatar, price }) {
     return (
       <div>
-        <img src={data.url} alt={data.title} />
-        <h2>{data.title}</h2>
+        <img src={imgUrl} alt={title} />
+        <h2>{title}</h2>
         <p>
-          Author: <a href={'data.author.url'}>{data.author.tag}</a>
+          Author: <a href={imgUrl}>{avatar},{author}</a>
         </p>
-        <p>Price: {data.price} credits</p>
-        <p>Quantity: {data.quantity}</p>
+        <p>Price: {price} credits</p>
+        <p>Quantity: {quantity}</p>
         <button></button>
       </div>
     );
-  }
-  
-  ReactDOM.render(painting, document.querySelector('#root'));
-  
-  const example = example[0];
-  
-  ReactDOM.render(
-    <Painting
-      url={example.url}
-      a="x+2=5"
-      b="y-3=2"
-      c="z-(1+1)=3"
-      d={{ e: 7 }}
-      f={10}
-      hint="This is props."
-    />,
-    document.querySelector('#root')
-  );
+  };
+
+  export default Painting;
