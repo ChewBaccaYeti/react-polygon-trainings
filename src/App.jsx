f6d1e18f5d53ee734e744b54b3ca92ebaaa3e938
Venedykt <chewbaccayeti@gmail.com>
2023-09-06 19:56:36 +0300

    Mock API using, backend data recall, Formik using and modal window advantage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,86 @@
-import React, { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
-import { GlobalStyle } from 'components/GlobalStyle';
-import { Layout } from 'components/Layout';
+import { Component } from 'react';
+import { GlobalStyle } from 'utils/GlobalStyle';
+import { Layout } from 'utils/Layout';
+import { MaterialList } from 'components/MsterialList/MaterialList';
 import { MaterialEditorForm } from 'components/MaterialEditorForm/MaterialEditorForm';
 import * as API from 'services/api';
 
 export class App extends Component {
   state = {
     materials: [],
+    isLoading: false,
+    error: false,
+  };
+
+  async componentDidMount() {
+    try {
+      this.setState({ isLoading: true, error: false });
+      const materials = await API.getMaterial();
+      this.setState({ materials });
+    } catch (error) {
+      this.setState({ error: true });
+      console.error(error);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  addMaterial = async values => {
+    try {
+      const material = await API.addMaterial(values);
+      this.setState(state => ({
+        materials: [...state.materials, material],
+      }));
+    } catch (error) {
+      this.setState({ error: true, isLoading: false });
+      console.log(error);
+    }
+  };
+
+  deleteMaterial = async id => {
+    try {
+      await API.deleteMaterial(id);
+      this.setState(state => ({
+        materials: state.materials.filter(material => material.id !== id),
+      }));
+    } catch (error) {
+      this.setState({ error: true });
+      console.log(error);
+    }
+  };
+
+  updateMaterial = async fields => {
+    try {
+      const updatedMaterial = await API.updateMaterial(fields);
+      this.setState(state => ({
+        materials: state.materials.map(material =>
+          material.id === fields.id ? updatedMaterial : material
+        ),
+      }));
+    } catch (error) {
+      this.setState({ error: true });
+      console.log(error);
+    }
   };
 
   render() {
+    const { materials, isLoading, error } = this.state;
     return (
       <Layout>
         <GlobalStyle />
-        <MaterialEditorForm onSubmit={console.log} />
+        {error && (
+          <p>Crap! Something get wrooong :( Reload the page and try again.</p>
+        )}
+        <MaterialEditorForm onSubmit={this.addMaterial} />
+        {isLoading ? (
+          'Loading'
+        ) : (
+          <MaterialList
+            items={materials}
+            onDelete={this.deleteMaterial}
+            onUpdate={this.updateMaterial}
+          />
+        )}
       </Layout>
     );
   }
--- src/components/MaterialEditorForm/MaterialEditorForm.js
+++ src/components/MaterialEditorForm/MaterialEditorForm.js
@@ -1,26 +1,37 @@
 import { Formik, Form, Field } from 'formik';
+import styled from 'styled-components';
+import styles from './MEF.module.css';
+
+const Input = styled(Field)`
+  border: 3px solid orange;
+  border-radius: 3px;
+  font-size: 30px;
+`;
 
 export const MaterialEditorForm = ({ onSubmit }) => {
-  const handleSubmit = (values, actions) => {
-    onSubmit(values);
+  const handleSubmit = async (values, actions) => {
+    await onSubmit(values);
+    actions.setSubmitting(false);
     actions.resetForm();
   };
 
   return (
     <Formik initialValues={{ title: '', link: '' }} onSubmit={handleSubmit}>
-      <Form>
-        <label>
-          Discription:
-          <Field name="title" type="text" />
-        </label>
-        <br />
-        <label>
-          Link:
-          <Field name="title" type="url" />
-        </label>
-        <br />
-        <button type="submit">Add material.</button>
-      </Form>
+      {({ isSubmitting }) => (
+        <Form className={styles.form}>
+          <label className={styles.label}>
+            Discription:
+            <Input name="title" type="text" />
+          </label>
+          <label className={styles.label}>
+            Link:
+            <Field name="link" type="url" />
+          </label>
+          <button type="submit" disabled={isSubmitting}>
+            Add material
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
